7f8237abec14d47dad872060c3e28209d8e9aa26
Dan <dlithio@users.noreply.github.com>
2015-05-15 17:30:06 -0400

    Working lorenz examples with adapt and remove

--- driver.f90
+++ driver.f90
@@ -5,6 +5,7 @@ integer :: ndim
 integer :: npoints, npoints_start
 integer :: sdiff_switch
 integer :: t_switch
+integer :: f_switch
 integer :: ts_switch
 integer :: integral_switch
 double precision, allocatable :: eigvec1(:)
@@ -14,40 +15,44 @@ double precision :: eigval2
 double precision, allocatable :: fixed_point(:)
 double precision :: radius
 double precision :: dt
-double precision :: distance_percentage
+double precision :: distance_percentagefar
+double precision :: distance_percentageclose
 integer :: steps_per_save,stepnum
 integer :: saved_rings,ringnum
 logical :: something_wrong
+integer :: numchecks
 
 npoints_start = 128
 ndim = 3
 radius = 15.d0
-sdiff_switch = 1
+sdiff_switch = 2
 t_switch = 2
 ts_switch = 2
 integral_switch = 2
-dt = 46.d0
-steps_per_save = 1;
-saved_rings = 2;
-distance_percentage = 2.d0;
+dt = 1.d-2
+steps_per_save = 1000;
+saved_rings = 15;
+distance_percentagefar = 3.d0;
+distance_percentageclose = 0.25d0;
+f_switch = 3;
 
 npoints = npoints_start
-call set_switches(sdiff_switch,t_switch,ts_switch,integral_switch)
+call set_switches(sdiff_switch,t_switch,ts_switch,integral_switch,f_switch)
 call allocate_arrays(ndim,npoints)
 
 allocate(eigvec1(ndim))
 allocate(eigvec2(ndim))
 eigvec1 = (/ 1.d0, 0.d0, 0.d0 /)
 eigvec2 = (/ 0.d0, 1.d0, 0.d0 /)
-!eigvec1 = (/ 0.61482d0, -0.78867d0, 0.d0 /)
-!eigvec2 = (/ 0.d0, 0.d0, 1.d0 /)
+eigvec1 = (/ 0.61482d0, -0.78867d0, 0.d0 /)
+eigvec2 = (/ 0.d0, 0.d0, 1.d0 /)
 eigval1 = 1.d0
 eigval2 = 1.d0
 allocate(fixed_point(ndim))
 fixed_point = (/ 0.d0, 0.d0, 0.d0 /)
 call set_initial_points(eigvec1,eigvec2,eigval1,eigval2,fixed_point,radius,npoints)
 
-call find_fideal(ndim,npoints)
+call find_f(ndim,npoints)
 open(unit=101,file="header")
 write(101,*) ndim
 write(101,*) saved_rings
@@ -58,19 +63,24 @@ call deallocate_arrays()
 call allocate_arrays(ndim,npoints)
 call set_initial_points(eigvec1,eigvec2,eigval1,eigval2,fixed_point,radius,npoints)
 ringnum=1
-call set_when_to_adapt(radius,npoints,distance_percentage)
+call set_when_to_adapt(radius,npoints,distance_percentagefar,distance_percentageclose)
 call write_output(ringnum,npoints)
 
 do ringnum=2,saved_rings
     do stepnum=1,steps_per_save
-        call find_fideal(ndim,npoints)
+        call find_f(ndim,npoints)
         call timestep(dt)
         call check_new_ring(npoints,something_wrong)
+        numchecks = 0
         do while (something_wrong)
             call fix_old_ring(ndim,npoints)
-            call find_fideal(ndim,npoints)
+            call find_f(ndim,npoints)
             call timestep(dt) 
             call check_new_ring(npoints,something_wrong)
+            numchecks = numchecks + 1
+            if (numchecks .gt. 1000) then
+                write(*,*) 'hi'
+            endif
         enddo
         call accept_new_ring()
     enddo
--- ring.f90
+++ ring.f90
@@ -3,10 +3,12 @@ use FFTW3
 implicit none
 double precision, private :: pi=4.d0*datan(1.d0)
 double precision, private :: max_dist
+double precision, private :: min_dist
 integer, private :: sdiff_switch
 integer, private :: t_switch
 integer, private :: ts_switch
 integer, private :: integral_switch
+integer, private :: f_switch
 integer :: big
 double precision, allocatable, private :: points(:,:)
 double precision, allocatable, private :: big_points(:,:)
@@ -37,25 +39,29 @@ complex(kind=8), allocatable, private :: fftw_integral(:)
 logical, private :: first_run = .TRUE.
 contains
 
-subroutine set_switches(sdiff_switch_input,t_switch_input,ts_switch_input,integral_switch_input)
+subroutine set_switches(sdiff_switch_input,t_switch_input,ts_switch_input,integral_switch_input,f_switch_input)
 implicit none
 integer, intent(in) :: sdiff_switch_input
 integer, intent(in) :: t_switch_input
 integer, intent(in) :: ts_switch_input
 integer, intent(in) :: integral_switch_input
+integer, intent(in) :: f_switch_input
 sdiff_switch = sdiff_switch_input
 t_switch = t_switch_input
 ts_switch = ts_switch_input
 integral_switch = integral_switch_input
+f_switch = f_switch_input
 big = 1
 end subroutine set_switches
 
-subroutine set_when_to_adapt(radius,npoints,distance_percentage)
+subroutine set_when_to_adapt(radius,npoints,distance_percentagefar,distance_percentageclose)
 implicit none
-double precision, intent(in) :: distance_percentage
+double precision, intent(in) :: distance_percentagefar
+double precision, intent(in) :: distance_percentageclose
 double precision, intent(in) :: radius
 integer, intent(in) :: npoints
-max_dist = 2*pi*radius/dble(npoints)*distance_percentage
+max_dist = 2*pi*radius/dble(npoints)*distance_percentagefar
+min_dist = 2*pi*radius/dble(npoints)*distance_percentageclose
 !write(*,*) max_dist
 end subroutine set_when_to_adapt
 
@@ -317,12 +323,20 @@ call find_distance(points_new,dist_diff,npoints)
 something_wrong = (something_wrong .OR. any(dist_diff .gt. max_dist))
 end subroutine check_points_far
 
+subroutine check_points_close(npoints,something_wrong)
+implicit none
+integer, intent(in) :: npoints
+logical, intent(inout) :: something_wrong
+something_wrong = (something_wrong .OR. any(dist_diff .lt. min_dist))
+end subroutine check_points_close
+
 subroutine check_new_ring(npoints,something_wrong)
 implicit none
 integer, intent(in) :: npoints
 logical, intent(inout) :: something_wrong
 something_wrong = .FALSE.
 call check_points_far(npoints,something_wrong)
+call check_points_close(npoints,something_wrong)
 end subroutine check_new_ring
 
 subroutine accept_new_ring()
@@ -337,28 +351,55 @@ integer, intent(inout) :: npointsold
 integer :: new_npoints
 integer :: old_point
 integer :: new_point
-deallocate(points_new)
-deallocate(position_vec_new)
-new_npoints = npointsold + count(dist_diff .gt. max_dist)
-allocate(points_new(ndim,new_npoints))
-allocate(position_vec_new(new_npoints))
-new_point = 1
-old_point = 1
-do old_point=1,npointsold
-    if (dist_diff(old_point) .gt. max_dist) then
-        call interpolate(points,points_new,old_point,new_point,npointsold)
-        call interpolatepos(position_vec,position_vec_new,old_point,new_point,npointsold)
+logical :: made_change
+made_change = .false.
+if (any(dist_diff .gt. max_dist)) then
+    made_change = .true.
+    ! First interpolate where the points are too far away
+    deallocate(points_new)
+    deallocate(position_vec_new)
+    new_npoints = npointsold + count(dist_diff .gt. max_dist)
+    allocate(points_new(ndim,new_npoints))
+    allocate(position_vec_new(new_npoints))
+    new_point = 1
+    old_point = 1
+    do old_point=1,npointsold
+        if (dist_diff(old_point) .gt. max_dist) then
+            call interpolate(points,points_new,old_point,new_point,npointsold)
+            call interpolatepos(position_vec,position_vec_new,old_point,new_point,npointsold)
+            new_point = new_point + 1
+        endif
+        points_new(:,new_point) = points(:,old_point)
+        position_vec_new(new_point) = position_vec(old_point)
         new_point = new_point + 1
-    endif
-    points_new(:,new_point) = points(:,old_point)
-    position_vec_new(new_point) = position_vec(old_point)
-    new_point = new_point + 1
-enddo
-call deallocate_arrays()
-call allocate_arrays(ndim,new_npoints)
-npointsold = new_npoints
-points = points_new
-position_vec = position_vec_new
+    enddo
+    call deallocate_arrays()
+    call allocate_arrays(ndim,new_npoints)
+    npointsold = new_npoints
+    points = points_new
+    position_vec = position_vec_new
+endif
+if (any(dist_diff .lt. min_dist) .and. (.not. made_change)) then
+    ! Now remove points that are too close
+    deallocate(points_new)
+    deallocate(position_vec_new)
+    new_npoints = npointsold - count(dist_diff .lt. min_dist)
+    allocate(points_new(ndim,new_npoints))
+    allocate(position_vec_new(new_npoints))
+    new_point = 1
+    do old_point=1,npointsold
+        if (dist_diff(old_point) .ge. min_dist) then
+            points_new(:,new_point) = points(:,old_point)
+            position_vec_new(new_point) = position_vec(old_point)
+            new_point = new_point + 1
+        endif
+    enddo
+    call deallocate_arrays()
+    call allocate_arrays(ndim,new_npoints)
+    npointsold = new_npoints
+    points = points_new
+    position_vec = position_vec_new
+endif
 end subroutine fix_old_ring
 
 subroutine interpolate(oldarray,newarray,old_point,new_point,npointsold)
@@ -461,6 +502,49 @@ second_integral = s/(2.d0*pi)*first_integral(npoints)
 !write(*,*) 'f_dot_ts',f_dot_ts
 !write(*,*) 'second_integral',second_integral
 end subroutine find_second_integral
+
+subroutine find_f(ndim,npoints)
+implicit none
+integer, intent(in) :: ndim
+integer, intent(in) :: npoints
+if (f_switch .eq. 1) then
+call find_fideal(ndim,npoints)
+endif
+if (f_switch .eq. 2) then
+call find_fzero(ndim,npoints)
+endif
+if (f_switch .eq. 3) then
+call find_forig(ndim,npoints)
+endif
+end subroutine find_f
+
+subroutine find_forig(ndim,npoints)
+implicit none
+integer, intent(in) :: ndim
+integer, intent(in) :: npoints
+integer :: i
+call points_to_f(ndim,npoints)
+call find_sdiff(npoints)
+call find_s(npoints,sdiff,s)
+fideal = f
+end subroutine find_forig
+
+subroutine find_fzero(ndim,npoints)
+implicit none
+integer, intent(in) :: ndim
+integer, intent(in) :: npoints
+integer :: i
+call points_to_f(ndim,npoints)
+call find_sdiff(npoints)
+call find_s(npoints,sdiff,s)
+call points_to_tangent(ndim,npoints)
+call dot(t,f,f_dot_t,npoints)
+do i=1,npoints
+    fideal(:,i) = f(:,i) - f_dot_t(i) * t(:,i)
+!    write(*,*) '**',i,fideal(:,i),i,'**'
+enddo
+call normc(fideal,npoints)
+end subroutine find_fzero
 
 subroutine find_fideal(ndim,npoints)
 implicit none
