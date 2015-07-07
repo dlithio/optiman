25114e7397df74dd8e6c5e7cf1447c75a289a6b5
Dan <dlithio@users.noreply.github.com>
2015-07-07 17:14:15 -0400

    Improved simple demo settings and jacobian

--- demos/simple/simple_example.f90
+++ demos/simple/simple_example.f90
@@ -25,17 +25,12 @@ double precision, intent(inout) :: x( 1: )
 double precision, intent(inout) :: a( 1: , 1: )
 double precision :: original_force(n),forward_state(n),forward_force(n)
 integer :: i
-double precision :: h
-h = 1.d-4
 a = 0.d0
-call fcn ( n, x, original_force)
-do i=1,n
-    forward_state = x
-    forward_state(i) = forward_state(i) + h
-    call fcn ( n, forward_state, forward_force)
-    forward_force = (forward_force - original_force)/h
-    a(:,i) = forward_force
-enddo
+a(1,1) = 2.d0
+a(2,2) = 2.d0
+a(3,3) = -1.d0
+a(3,1) = 2.d0*dcos(x(1))-2.d0*x(1)*dsin(x(1))+dcos(x(1))
+a(3,2) = 2.d0*dcos(x(2))-2.d0*x(2)*dsin(x(2))+dcos(x(2))
 end subroutine get_jac
 
 end module
